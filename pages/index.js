4ff1f6e2d67e1476812433ae6cbfadd859e2010e
Kyle Anderson <kmanderson12@gmail.com>
2019-11-06 20:57:09 -0600

    habits query and refetching

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { withApollo } from '../lib/apollo';
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
@@ -6,23 +5,14 @@ import Layout from '../components/Layout';
 import HabitList from '../components/HabitList';
 import HabitForm from '../components/HabitForm';
 
-const HELLO_QUERY = gql`
-  query HelloQuery {
-    sayHello
-  }
-`;
-
 const Home = () => {
-  const { data, loading, error } = useQuery(HELLO_QUERY);
-  const [habits, setHabits] = useState(['Do the dishes']);
-  if (loading) return <div />;
   return (
     <Layout>
       <div className="hero">
         <h1 className="title">Level Up Your Life</h1>
         <div className="list">
-          <HabitForm setHabits={setHabits} />
-          <HabitList habits={habits} />
+          <HabitForm />
+          <HabitList />
         </div>
       </div>
 
